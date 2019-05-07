63f085bf453bab9232a59c5399e24b3551e202dc
Emily R <emilyroulund@gmail.com>
2019-05-07 00:00:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,7 +46,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestants|
     contestants.each do |info|
       i += 1
-      total_age =+ info["age"].to_f
+      total_age =+ contestants["age"].to_f
     end
     total_age/i
   end
